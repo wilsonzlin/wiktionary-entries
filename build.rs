390e0b01145b959f36b3cd72cdb164e3d057494f
Wilson Lin <code@wilsonl.in>
2022-11-12 15:54:24 +1100

    Download in build.rs

--- build.rs
+++ build.rs
@@ -1,21 +1,37 @@
-use std::{fs::File, io::{BufReader, Write, BufRead}, env::var};
+use flate2::read::GzDecoder;
+use reqwest::blocking::get;
+use std::env::var;
+use std::fs::File;
+use std::io::Read;
+use std::io::Write;
 
 fn main() {
-  let file = File::open("entries.txt").unwrap();
-  let reader = BufReader::new(file);
+  let res = get("https://dumps.wikimedia.org/enwiktionary/20221101/enwiktionary-20221101-all-titles-in-ns0.gz")
+    .unwrap()
+    .error_for_status()
+    .unwrap()
+    .bytes()
+    .unwrap()
+    .to_vec();
+  let mut d = GzDecoder::new(&res[..]);
+  let mut lines = String::new();
+  d.read_to_string(&mut lines).unwrap();
   let mut out = File::create(format!("{}/entries.rs", var("OUT_DIR").unwrap())).unwrap();
   out.write_all(b"pub const ENTRIES: &[&str] = &[").unwrap();
-  for line in reader.lines() {
-    let line = line.unwrap();
+  for line in lines.split('\n') {
+    let line = line.trim();
+    if line.is_empty() {
+      continue;
+    };
     // TODO Maybe exclude non-English words?
     // TODO Remove typo entries.
     if line.chars().any(|c| match c {
       '-' | '?' | '!' | '_' | '"' | ' ' | '\\' | '^' => true,
-      _ => false
+      _ => false,
     }) {
       continue;
-    }
+    };
     write!(out, "  \"{}\",\n", line).unwrap();
-  }
+  };
   out.write_all(b"];\n").unwrap();
 }
--- download.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -Eeuo pipefail
-
-curl -fLSs https://dumps.wikimedia.org/enwiktionary/20221101/enwiktionary-20221101-all-titles-in-ns0.gz | gzip -c -d - > entries.txt
